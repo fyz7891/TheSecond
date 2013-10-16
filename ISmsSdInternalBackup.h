6e27b58b926b80e87eb5e0e1c84036e02c0b9c99
fyz7891 <fyz7891@126.com>
2013-10-16 18:31:48 +0800

    Sympto chaneg Bug ID: dd Root Cause:aa Solution: a Note:aa
    

---
+++ ISmsSdInternalBackup.h
@@ -0,0 +1,37 @@
+#ifndef __ISD_INTERNAL_BACKUP_H__
+#define __ISD_INTERNAL_BACKUP_H__
+
+#include <gaia/core/Context.h>
+#include <gaia/base/String.h>
+#include "sdcardbackup/IProgressUpdater.h"
+#include "sdcardbackup/StubSdInternalInterface.h"
+
+using namespace gaia::base;
+class ISmsSdInternalBackup: public StubSdInternalInterface {
+public:
+	ISmsSdInternalBackup(gaia::core::Context *pContext);
+    virtual ~ISmsSdInternalBackup();
+    
+    virtual int32_t onBackup( gaia::base::String folderPath );
+
+    virtual int32_t onRestore( gaia::base::String folderPath, int32_t restoreCount );
+
+    virtual void onCancel();
+
+    virtual int32_t getBackupCount();
+    
+    virtual gaia::base::String getAgentSpecificErrorString(int32_t errorCode);
+
+    virtual void setCallback( IProgressUpdater* callback );
+
+	void ensureDirectory(String dirName);
+	bool fileExist(String dirName, String fileName);
+	int readVMsg(String filePath);
+private:
+
+    gaia::core::Context *mpContext;
+    IProgressUpdater* mpProgressUpdater;
+    bool mCancel;
+};
+
+#endif  // __ISD_INTERNAL_BACKUP_H__
